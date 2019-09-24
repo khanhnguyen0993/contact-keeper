a327a057644706954629451cf51024da93dc085b
Khanh <khanhnguyen0993@gmail.com>
2019-09-24 23:41:30 +1000

    User Model & Validation

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const router = express.Router();
+// const {check, validationResult} = require('express-validator/check'); -> deprecated
+const {check, validationResult} = require('express-validator');
+
+const User = require('../models/User');
 
 //  @route        POST api/users -> '/'
 //  @desc         Register a user
 //  @access       Public 
-router.post('/', (req, res) => {
-  res.send('Register a user');
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Enter a valid email').isEmail(),
+    check('password', 'Enter a password with 6 or more characters').isLength({
+      min: 6
+    })
+  ],(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({errors: errors.array()});
+    }
+
+    res.send('passed');  
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect database
+connectDB();
+
+// Init Middleware -> accept body data
+app.use(express.json({extended:false}));
 
 app.get('', (req, res) => res.json({msg: 'Welcome to the ContactKeeper API...'}));
 
